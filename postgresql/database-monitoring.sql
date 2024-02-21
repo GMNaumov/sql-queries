8cb49bb3ea5968582a59640c2f13bdc23c244458
Georgii Naumov <gmnaumov@yandex.ru>
2024-02-21 21:38:08 +0300

    feat: add postgresql db monitoring queries

--- postgresql/database-monitoring.sql
+++ postgresql/database-monitoring.sql
@@ -1,2 +1,11 @@
 --Show all active connections
 SELECT * FROM pg_stat_activity;
+
+--Show all tables
+SELECT * FROM pg_catalog.pg_tables;
+
+--Show all views
+SELECT * FROM pg_catalog.pg_views;
+
+--Show all roles
+SELECT * FROM pg_catalog.pg_roles;
