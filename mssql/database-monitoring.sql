647150d2e12418c1d79670831530bd1ddaf7bddf
Georgii Naumov <gmnaumov@yandex.ru>
2024-02-24 22:44:11 +0300

    feat: Add ms sql query for duplicate specific table

--- mssql/database-monitoring.sql
+++ mssql/database-monitoring.sql
@@ -1,2 +1,8 @@
 --Show all active processes
 EXEC sp_who2 'active';
+
+--Duplicate a table between two databases
+SELECT * INTO 
+    [DATABASE_NAME].[SCHEMA_NAME].[NEW_TABLE_NAME] 
+FROM 
+    [DATABASE_NAME].[SCHEMA_NAME].[OLD_TABLE_NAME]
